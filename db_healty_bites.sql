c2ae64a6f3adb719910999057d24e22277bf26d1
kgdr26 <kangdru261199@gmail.com>
2023-05-11 08:28:05 +0700

    update db

--- db_healty_bites.sql
+++ db_healty_bites.sql
@@ -36,6 +36,27 @@ insert  into `mst_category`(`id`,`name`,`is_active`,`update_by`,`last_update`) v
 (2,'Snack',1,1,'2023-04-20 16:51:17'),
 (3,'salads',1,1,'2023-04-20 17:44:25');
 
+/*Table structure for table `mst_contentlayanan` */
+
+DROP TABLE IF EXISTS `mst_contentlayanan`;
+
+CREATE TABLE `mst_contentlayanan` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_layanan` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `content` text DEFAULT NULL,
+  `is_active` int(11) DEFAULT NULL,
+  `update_by` int(11) DEFAULT NULL,
+  `last_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `mst_contentlayanan` */
+
+insert  into `mst_contentlayanan`(`id`,`id_layanan`,`name`,`content`,`is_active`,`update_by`,`last_update`) values 
+(1,1,'Tes dingin','Tesnnnmndsnsdncn skjd vcj xdscvm xvdvsdvdsvdfbhgf',1,1,'2023-05-10 14:43:46'),
+(2,2,'ggggggg','dfgdfgdfbgdfbgdfbdfbfddfdfxdzsfvb',1,1,'2023-05-10 14:44:07');
+
 /*Table structure for table `mst_layanan` */
 
 DROP TABLE IF EXISTS `mst_layanan`;
@@ -76,7 +97,7 @@ CREATE TABLE `mst_product` (
 
 insert  into `mst_product`(`id`,`id_category`,`name`,`price`,`description`,`img`,`is_active`,`update_by`,`last_update`) values 
 (1,1,'Bold Bulgogi',15000,'Lagi kebanyakan makan daging melulu dari kemarin, dan malam ini waktunya go green dulu ????\r\nSaya order “Bold Bulgogi” yg harganya 15k. Nikmat banget nih! Semangkuk full isi berbagai macam sayuran dengan potongan daging bulgogi khas korea. MUST!','contoh_1.png',1,1,'2023-05-03 19:06:59'),
-(2,2,'Tes',30000,'bjkjkj',NULL,1,1,'2023-05-07 09:49:18'),
+(2,2,'Tes',30000,'bjkjkj','71493.png',1,1,'2023-05-10 15:29:35'),
 (3,1,'Cobb',40000,'Makananan enak sehati bergizi murah pula harga nya','71493.png',1,1,'2023-05-07 09:53:04');
 
 /*Table structure for table `mst_role` */
@@ -98,6 +119,25 @@ insert  into `mst_role`(`id`,`name`,`is_active`,`update_by`,`last_update`) value
 (1,'ADMINISTRATOR',1,1,'2023-04-14 14:09:18'),
 (2,'CUSTOMER',1,1,'2023-04-15 19:29:52');
 
+/*Table structure for table `mst_sett_home` */
+
+DROP TABLE IF EXISTS `mst_sett_home`;
+
+CREATE TABLE `mst_sett_home` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `text` mediumtext DEFAULT NULL,
+  `update_by` int(11) DEFAULT NULL,
+  `last_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `mst_sett_home` */
+
+insert  into `mst_sett_home`(`id`,`text`,`update_by`,`last_update`) values 
+(1,'Tex dibagian ini sudah terhubung dengan database jadi bisa di setting di role nya admin menu setting. Tex dibagian ini sudah terhubung dengan database jadi bisa di setting di role nya admin menu setting. Tex dibagian ini sudah terhubung dengan database jadi bisa di setting di role nya admin menu setting. Tex dibagian ini sudah terhubung dengan database jadi bisa di setting di role nya admin menu setting. Tex dibagian ini sudah terhubung dengan database jadi bisa di setting di role nya admin menu setting. Tex dibagian ini sudah terhubung dengan database jadi bisa di setting di role nya admin menu setting.',1,'2023-05-10 15:37:25'),
+(2,'Tex dibagian ini sudah terhubung dengan database jadi bisa di setting di role nya admin menu setting. Tex dibagian ini sudah terhubung dengan database jadi bisa di setting di role nya admin menu setting. Tex dibagian ini sudah terhubung dengan database jadi bisa di setting di role nya admin menu setting. Tex dibagian ini sudah terhubung dengan database jadi bisa di setting di role nya admin menu setting. Tex dibagian ini sudah terhubung dengan database jadi bisa di setting di role nya admin menu setting. Tex dibagian ini sudah terhubung dengan database jadi bisa di setting di role nya admin menu setting.',1,'2023-05-10 15:38:09'),
+(3,'Tex dibagian ini sudah terhubung dengan database jadi bisa di setting di role nya admin menu setting. Tex dibagian ini sudah terhubung dengan database jadi bisa di setting di role nya admin menu setting. ',1,'2023-05-10 15:39:39');
+
 /*Table structure for table `users` */
 
 DROP TABLE IF EXISTS `users`;
@@ -115,14 +155,16 @@ CREATE TABLE `users` (
   `update_by` int(11) DEFAULT NULL,
   `last_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 /*Data for the table `users` */
 
 insert  into `users`(`id`,`username`,`password`,`pass`,`role_id`,`name`,`email`,`foto`,`is_active`,`update_by`,`last_update`) values 
 (1,'kgdr','$2y$10$XUvLMfGJPSkg5KzRGdc/t.f/Auw2xwQEPUburLlfInct9dDFCvm7q','1',1,'Kang Dru','kgdr@gmail.com','default.jpg',1,1,'2023-04-15 19:37:45'),
 (2,'adm','$2y$10$XUvLMfGJPSkg5KzRGdc/t.f/Auw2xwQEPUburLlfInct9dDFCvm7q','1',1,'Admin','adm@gmail.com','default.jpg',1,1,'2023-04-15 19:37:52'),
-(3,'usr','$2y$10$Kb5Cm.gR4mqcFsaKzmmWDOM0FbxIDjqCmNm4PhaQqyzUug8wLM5Va','1',2,'Tes usr','usr@gmail.com','80763.png',1,1,'2023-04-20 13:03:03');
+(3,'usr','$2y$10$Kb5Cm.gR4mqcFsaKzmmWDOM0FbxIDjqCmNm4PhaQqyzUug8wLM5Va','1',2,'Tes usr','usr@gmail.com','80763.png',1,1,'2023-04-20 13:03:03'),
+(4,'ts1','$2y$10$v2zW79n83Myb5b4FqnFwD.60mRvfMTkjs1NSd7BrW3dsJAjYAp0Xm','5',2,'fgfg','gfg','default.jpg',1,1,'2023-05-10 11:26:03'),
+(5,'ts2','$2y$10$3LpQZJs3nxD0HOaTfSX81..lY0COorTPmQryCy25eyI2uos0mnnTO','1',2,'tes regis','dgfsdgdf','default.jpg',1,1,'2023-05-10 11:26:07');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
