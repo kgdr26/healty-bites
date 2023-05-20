d39d15a229a597e7adcda54cb0d1870c72411457
kgdr26 <kangdru261199@gmail.com>
2023-05-20 12:48:40 +0700

    db nya

--- db_healty_bites.sql
+++ db_healty_bites.sql
@@ -35,7 +35,7 @@ CREATE TABLE `mst_bahan` (
   `update_by` int(11) DEFAULT NULL,
   `last_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 /*Data for the table `mst_bahan` */
 
@@ -44,7 +44,10 @@ insert  into `mst_bahan`(`id`,`name`,`serving`,`energy`,`protein`,`fat`,`carbohy
 (2,'Red & White Cabbage','150','37.0','2.0','0','9.0','57143.jpg',2,1000,1,1,'2023-05-13 06:59:00'),
 (3,'Avocado','40','64.0','1.0','6.0','3.0','17536.jpg',3,5000,1,1,'2023-05-13 07:03:16'),
 (4,'Honey Mustard','5','10.5','0','1.0','0.5','87182.jpg',4,5000,1,1,'2023-05-13 07:26:46'),
-(5,'Cheddar','5','10.5','0','1.5','0.5','11268.jpg',3,5000,1,1,'2023-05-13 07:56:43');
+(5,'Cheddar','5','10.5','0','1.5','0.5','11268.jpg',3,5000,1,1,'2023-05-13 07:56:43'),
+(6,'Tomato Wrap','100','14.0','4.0','3.0','23.0','59044.png',1,5000,1,1,'2023-05-20 09:00:35'),
+(7,'Spinach Wrap','100','100.0','2.0','1.0','20.0','93360.png',1,5000,1,1,'2023-05-20 09:01:17'),
+(8,'Regular Wrap','100','101.0','3.0','2.0','18.0','15868.jpg',1,5000,1,1,'2023-05-20 09:01:56');
 
 /*Table structure for table `mst_category` */
 
@@ -115,16 +118,19 @@ CREATE TABLE `mst_product` (
   `greens` varchar(255) DEFAULT NULL,
   `toppings` varchar(255) DEFAULT NULL,
   `dressings` varchar(255) DEFAULT NULL,
+  `wishlist` text DEFAULT NULL,
   `is_active` int(11) DEFAULT NULL,
   `update_by` int(11) DEFAULT NULL,
   `last_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 /*Data for the table `mst_product` */
 
-insert  into `mst_product`(`id`,`id_category`,`name`,`price`,`description`,`img`,`bread`,`greens`,`toppings`,`dressings`,`is_active`,`update_by`,`last_update`) values 
-(1,1,'Hail Caesar',45000,'Enak, Nikmat, & Bergizi , poko nya enak banget dah murah pula berkualitas','51073.png','[]','[\"1\",\"2\"]','[\"3\",\"5\"]','[\"4\"]',1,1,'2023-05-13 10:58:20');
+insert  into `mst_product`(`id`,`id_category`,`name`,`price`,`description`,`img`,`bread`,`greens`,`toppings`,`dressings`,`wishlist`,`is_active`,`update_by`,`last_update`) values 
+(1,1,'Hail Caesar',45000,'Enak, Nikmat, & Bergizi , poko nya enak banget dah murah pula berkualitas','51073.png','[]','[\"1\",\"2\"]','[\"3\",\"5\"]','[\"4\"]','[\"1\",\"2\",\"6\"]',1,1,'2023-05-20 10:02:31'),
+(2,2,'Tuna San',20000,'Nutritional information may vary depending on portioning.','58861.png','[\"7\"]','[\"1\"]','[\"3\"]','[\"4\"]','[]',1,1,'2023-05-20 09:54:10'),
+(3,2,'Cobb',30000,'Enak pok nya enbak dah poko nya enak enak','28435.png','[\"8\"]','[\"2\"]','[\"5\"]','[\"4\"]','[]',1,1,'2023-05-20 12:23:08');
 
 /*Table structure for table `mst_role` */
 
@@ -213,7 +219,7 @@ CREATE TABLE `mst_tahap_order` (
   `update_by` int(11) DEFAULT NULL,
   `last_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 /*Data for the table `mst_tahap_order` */
 
@@ -223,7 +229,9 @@ insert  into `mst_tahap_order`(`id`,`name`,`is_active`,`update_by`,`last_update`
 (3,'Delivery',1,1,'2023-05-14 07:46:14'),
 (4,'Waiting for payment',1,1,'2023-05-14 07:46:45'),
 (5,'Accepted',1,1,'2023-05-14 07:47:03'),
-(6,'Close',1,1,'2023-05-14 07:47:09');
+(6,'Order Received',1,1,'2023-05-20 11:55:56'),
+(7,'Waiting Approve Payment',1,1,'2023-05-20 11:24:42'),
+(8,'Cancel',1,1,'2023-05-20 11:48:11');
 
 /*Table structure for table `trx_order` */
 
@@ -244,15 +252,16 @@ CREATE TABLE `trx_order` (
   `id_user` int(11) DEFAULT NULL,
   `bukti_pembayaran` varchar(255) DEFAULT NULL,
   KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 /*Data for the table `trx_order` */
 
 insert  into `trx_order`(`id`,`kode`,`id_product`,`jns_product`,`qty`,`harga`,`order_methode`,`payment_methode`,`id_meja`,`id_tahap_order`,`tgl_order`,`id_user`,`bukti_pembayaran`) values 
-(0,'ORD.2023.05.0001',1,1,5,225000,1,NULL,NULL,2,'2023-05-14 02:31:59',6,NULL),
+(0,'ORD.2023.05.0001',1,1,5,225000,1,NULL,NULL,6,'2023-05-14 02:31:59',6,NULL),
 (2,'ORD.2023.05.0002',1,1,5,225000,2,NULL,4,1,'2023-05-14 02:32:51',6,NULL),
-(3,'ORD.2023.05.0003',1,1,10,450000,3,2,NULL,2,'2023-05-14 02:33:31',6,'22653.png'),
-(4,'ORD.2023.05.0004',1,1,60,2700000,2,NULL,8,1,'2023-05-14 03:10:01',6,NULL);
+(3,'ORD.2023.05.0003',1,1,10,450000,3,2,NULL,3,'2023-05-14 02:33:31',6,'52578.png'),
+(4,'ORD.2023.05.0004',1,1,60,2700000,2,NULL,8,8,'2023-05-14 03:10:01',6,NULL),
+(5,'ORD.2023.05.0005',2,1,100,2000000,3,2,NULL,4,'2023-05-20 04:30:19',6,NULL);
 
 /*Table structure for table `users` */
 
